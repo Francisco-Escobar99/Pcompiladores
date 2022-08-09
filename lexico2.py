18cf37021f3145665138661b91402fa0e4ff0e78
Gaby Gomez <gab.gomez032@gmail.com>
2022-08-09 16:06:22 -0500

    SD-05: Busqueda de Delimitadores

--- lexico2.py
+++ lexico2.py
@@ -1,6 +1,8 @@
 from gramatica import gramatica as grama
 
 tamanio_Reservadas = len(grama.reservadas)
+tamanio_Delimitadores = len(grama.delimitadores)
+tamanio_Operadores = len(grama.operadores)
 
 class lexico:
 
@@ -34,14 +36,66 @@ class lexico:
         for valor in range(len(tablaFinal)):
             totalFrecuencia = totalFrecuencia + tablaFinal[valor][1]
                 
-        print(texto)
-        print(newReservadas)
-        print(tablaFinal)
-        print(totalFrecuencia)
+        # print(texto)
+        # print(newReservadas)
+        # print(tablaFinal)
+        # print(totalFrecuencia)
 
         return texto, reservadas, totalFrecuencia
+    
+    def busqueda_Delimitadores(texto):
+        texto = texto.split()
+        tamanio_Texto = len(texto)
+        
+        total_Frecuencia = 0
+        delimitadores = []
+        newDelimitadores = []
+        frecuencia_Delimitadores = []
 
+        for _ in range(tamanio_Texto):
+            for numero in range(tamanio_Delimitadores):
+                if grama.delimitadores[numero] in texto:
+                    encontrada = grama.delimitadores[numero]
+                    newDelimitadores.append(encontrada)
+                    posicion = texto.index(encontrada)
+                    texto.pop(posicion)
+        
+        for simbolo in newDelimitadores:
+            frecuencia_Delimitadores.append(newDelimitadores.count(simbolo))
+        
+        for delimitador in newDelimitadores:
+            if delimitador not in delimitadores:
+                delimitadores.append(delimitador)
+        
+        tablaFinal = zip(delimitadores,frecuencia_Delimitadores)
+        tablaFinal = tuple(tablaFinal)
+        tamanio = len(tablaFinal)
 
+        for valor in range(tamanio):
+            total_Frecuencia += tablaFinal[valor][1]
+            
+        
+        # print(texto)
+        # print(newDelimitadores)
+        # print(tablaFinal)
+        # print(total_Frecuencia)
+
+        return texto, delimitadores, total_Frecuencia
+
+    
+    def busqueda_Operadores(texto):
+        pass
+
+    def busqueda_Variables(texto):
+        pass
+
+    def busqueda_Digitos(texto):
+        pass
+        
+        
+        
+        
+lexico.busqueda_Delimitadores('Hola == como estas < >=')
 # lexico.busqueda_Reservadas('Proceso numero , Caracter Real 34 Real 77 , Hacer Logico Real Logico Mexico FinProceso')
             
         
