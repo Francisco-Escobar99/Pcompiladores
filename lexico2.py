a57f86a9ad7aee5cb55ee27acbc532db65ee8ef4
Gaby Gomez <gab.gomez032@gmail.com>
2022-08-10 15:14:43 -0500

    SD-05: Actualizacion de la busqueda de los Delimitadores

--- lexico2.py
+++ lexico2.py
@@ -1,9 +1,6 @@
 from gramatica import gramatica as grama
 
 
-tamanio_Delimitadores = len(grama.delimitadores)
-tamanio_Operadores = len(grama.operadores)
-
 class lexico:
 
     def busqueda_General(texto, tamanio, t_Gramatica):
@@ -46,42 +43,15 @@ class lexico:
         return n_Texto, reservadas, frecuencia
     
     def busqueda_Delimitadores(texto):
-        tamanio_Texto = len(texto)
+        tamanio_Delimitadores = len(grama.delimitadores)
+        n_Texto, delimitadores, frecuencia = lexico.busqueda_General(texto, tamanio_Delimitadores, grama.delimitadores)
         
-        total_Frecuencia = 0
-        delimitadores = []
-        newDelimitadores = []
-        frecuencia_Delimitadores = []
-
-        for _ in range(tamanio_Texto):
-            for numero in range(tamanio_Delimitadores):
-                if grama.delimitadores[numero] in texto:
-                    encontrada = grama.delimitadores[numero]
-                    newDelimitadores.append(encontrada)
-                    posicion = texto.index(encontrada)
-                    texto.pop(posicion)
-        
-        for simbolo in newDelimitadores:
-            frecuencia_Delimitadores.append(newDelimitadores.count(simbolo))
-        
-        for delimitador in newDelimitadores:
-            if delimitador not in delimitadores:
-                delimitadores.append(delimitador)
-        
-        tablaFinal = zip(delimitadores,frecuencia_Delimitadores)
-        tablaFinal = tuple(tablaFinal)
-        tamanio = len(tablaFinal)
-
-        for valor in range(tamanio):
-            total_Frecuencia += tablaFinal[valor][1]
-
-        return texto, delimitadores, total_Frecuencia
+        return n_Texto, delimitadores, frecuencia
 
     
     def busqueda_Operadores(texto):
         tamanio_Texto = len(texto)
-
-        total_Frecuencias = 0 
+        tamanio_Operadores = len(grama.operadores)
 
     def busqueda_Variables(texto):
         pass
