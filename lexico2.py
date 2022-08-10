557f3463039dc229413b4f4a843cf6490ef3f143
Gaby Gomez <gab.gomez032@gmail.com>
2022-08-10 13:07:47 -0500

    SD-04: Actualizacion de la funcion busqueda de palabras reservadas

--- lexico2.py
+++ lexico2.py
@@ -1,50 +1,51 @@
 from gramatica import gramatica as grama
 
-tamanio_Reservadas = len(grama.reservadas)
+
 tamanio_Delimitadores = len(grama.delimitadores)
 tamanio_Operadores = len(grama.operadores)
 
 class lexico:
 
-    def busqueda_Reservadas(texto):
-        texto = texto.split()
+    def busqueda_General(texto, tamanio, t_Gramatica):
         tamanio_Texto = len(texto)
 
-        totalFrecuencia = 0
-        reservadas = []
-        newReservadas = []
-        frecuencia_Reservadas = []
-        
+        total_Frecuencia = 0
+        encontradas = []
+        nuevo_Texto = []
+        frecuencias = []
+
         for _ in range(tamanio_Texto):
-            for numero2 in range(tamanio_Reservadas):
-                if grama.reservadas[numero2] in texto:
-                    encontrada = grama.reservadas[numero2]
-                    newReservadas.append(encontrada)
+            for numero in range(tamanio):
+                if t_Gramatica[numero] in texto:
+                    encontrada = t_Gramatica[numero]
+                    nuevo_Texto.append(encontrada)
                     posicion = texto.index(encontrada)
                     texto.pop(posicion)
-                
-        for palabras in newReservadas:
-            frecuencia_Reservadas.append(newReservadas.count(palabras))
         
-        for palabra in newReservadas:
-            if palabra not in reservadas:
-                reservadas.append(palabra)
+        for palabras in nuevo_Texto:
+            frecuencias.append(nuevo_Texto.count(palabras))
+        
+        for palabra in nuevo_Texto:
+            if palabra not in encontradas:
+                encontradas.append(palabra)
+        
+        datos_Finales = zip(encontradas, frecuencias)
+        datos_Finales = tuple(datos_Finales)
 
-        tablaFinal = zip(reservadas,frecuencia_Reservadas)
-        tablaFinal = tuple(tablaFinal)
+        for valor in range(len(datos_Finales)):
+            total_Frecuencia += datos_Finales[valor][1]
+        
+        return texto, encontradas, total_Frecuencia
 
-        for valor in range(len(tablaFinal)):
-            totalFrecuencia = totalFrecuencia + tablaFinal[valor][1]
-                
-        # print(texto)
-        # print(newReservadas)
-        # print(tablaFinal)
-        # print(totalFrecuencia)
 
-        return texto, reservadas, totalFrecuencia
+    def busqueda_Reservadas(texto):
+        texto = texto.split()
+        tamanio_Reservadas = len(grama.reservadas)
+        n_Texto, reservadas, frecuencia = lexico.busqueda_General(texto, tamanio_Reservadas, grama.reservadas)
+
+        return n_Texto, reservadas, frecuencia
     
     def busqueda_Delimitadores(texto):
-        texto = texto.split()
         tamanio_Texto = len(texto)
         
         total_Frecuencia = 0
@@ -73,29 +74,18 @@ class lexico:
 
         for valor in range(tamanio):
             total_Frecuencia += tablaFinal[valor][1]
-            
-        
-        # print(texto)
-        # print(newDelimitadores)
-        # print(tablaFinal)
-        # print(total_Frecuencia)
 
         return texto, delimitadores, total_Frecuencia
 
     
     def busqueda_Operadores(texto):
-        pass
+        tamanio_Texto = len(texto)
+
+        total_Frecuencias = 0 
 
     def busqueda_Variables(texto):
         pass
 
     def busqueda_Digitos(texto):
         pass
-        
-        
-        
-        
-lexico.busqueda_Delimitadores('Hola == como estas < >=')
-# lexico.busqueda_Reservadas('Proceso numero , Caracter Real 34 Real 77 , Hacer Logico Real Logico Mexico FinProceso')
-            
         
