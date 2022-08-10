a8a9b4b9983531398a534f6c1aacddea55a36467
Gaby Gomez <gab.gomez032@gmail.com>
2022-08-10 15:01:29 -0500

    SD.04: Tercera actualización de la busqueda de palabras reservadas

--- gramatica.py
+++ gramatica.py
@@ -2,51 +2,26 @@
 class gramatica:
 
     reservadas = [
-        'Algortimo',
-        'Cadena',
         'Caracter',
         'Como',
-        'Con Paso',
-        'De Otro Modo',
         'Definir',
-        'Dimension',
         'Entero',
-        'Entonces',
         'Escribir',
         'Falso',
-        'FinAlgoritmo',
-        'FinFuncion',
-        'FinMientras',
-        'FinPara',
-        'FinSegun',
-        'FinSi',
-        'FinSubProceso',
-        'Funcion',
-        'Hacer',
-        'Hasta Que',
         'Leer',
         'Logico',
-        'Mientras',
-        'Para',
-        'Por Referencia',
-        'Por Valor',
         'Real',
-        'Segun',
-        'Si',
-        'Sin Saltar',
-        'SiNo',
-        'SubProceso',
         'Verdadero',
         'Proceso',
         'FinProceso'
     ]
 
     delimitadores = [
-        '==','!=','>','<','>=','<=','<>','[',']','"',','
+        '[',']','"',',','(',')','.'
     ]
 
     operadores = [
-        '+', '-', '*','/', '%','<-','(',')','&','|','~','^'
+        '+', '-', '*','/','<-'
     ]
 
     variables = '[a-zA-Z_][a-zA-Z_0-9]*'
